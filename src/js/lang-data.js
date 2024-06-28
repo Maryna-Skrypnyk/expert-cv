c4d59b217389835bd38495b6642a10f48577e85d
Maryna Skrypnyk <marina.skrypnyk@gmail.com>
2024-06-28 18:30:18 +0300

    adds files

---
+++ src/js/lang-data.js
@@ -0,0 +1,298 @@
+export const langArr = {
+  title: {
+    en: 'CV_Maryna S.',
+    ua: 'Марина Скрипник',
+  },
+  name: {
+    en: 'Maryna Skrypnyk',
+    ua: 'Марина Скрипник',
+  },
+  profession: {
+    en: 'Expert in financial and credit operations',
+    ua: 'Експерт з фінансово-кредитних операцій',
+  },
+  fullStack: {
+    en: 'Full Stack Developer',
+    ua: 'Full Stack розробник',
+  },
+  dob: {
+    en: 'DOB:',
+    ua: 'ДН:',
+  },
+  kyiv: {
+    en: 'Kyiv',
+    ua: 'Київ',
+  },
+  ukraine: {
+    en: 'Ukraine',
+    ua: 'Україна',
+  },
+  contacts: {
+    en: 'Contacts',
+    ua: 'Контакти',
+  },
+  technical: {
+    en: 'Technical Skills',
+    ua: 'Ключові компетенції',
+  },
+  techLaw: {
+    en: 'Understanding of legislation',
+    ua: 'Орієнтуюсь у законодавстві',
+  },
+  techDoc: {
+    en: 'Document management',
+    ua: 'Працюю з документами',
+  },
+  techOperations: {
+    en: 'Understanding of credit operations',
+    ua: 'Розуміюсь у кредитних операціях',
+  },
+  techAnalytic: {
+    en: 'Analytical skills',
+    ua: 'Маю аналітичні навики',
+  },
+  techTexts: {
+    en: 'Professional text writing',
+    ua: 'Пишу професійні тексти',
+  },
+  techPk: {
+    en: 'Proficiency in computer skills',
+    ua: 'Володію ПК',
+  },
+  techInet: {
+    en: 'Using internet resources',
+    ua: 'Використовую інтернет-ресурси',
+  },
+  soft: {
+    en: 'Soft Skills',
+    ua: 'Особисті якості',
+  },
+  proactive: {
+    en: 'Proactive',
+    ua: 'Проактивна',
+  },
+  motivation: {
+    en: 'Extreme self-motivation',
+    ua: 'Надзвичайно самомотивована',
+  },
+  enthusiasm: {
+    en: 'Enthusiasm and dedication',
+    ua: 'Виконую завдання з ентузіазмом і відданістю',
+  },
+  details: {
+    en: 'A pretty good eye for details',
+    ua: 'Уважна до деталей',
+  },
+  improve: {
+    en: 'Strive for improvement',
+    ua: 'Прагну до вдосконалення',
+  },
+  responsible: {
+    en: 'Responsible for own decisions',
+    ua: 'Відповідальна за свої рішення',
+  },
+  priorities: {
+    en: 'Set priorities',
+    ua: 'Розставляю пріоритети',
+  },
+  creative: {
+    en: 'Creative and vigorous',
+    ua: 'Креативна та енергійна',
+  },
+  outgoing: {
+    en: 'Outgoing',
+    ua: 'Товариська, привітна',
+  },
+  languages: {
+    en: 'Languages',
+    ua: 'Знання мов',
+  },
+  ukrainian: {
+    en: 'Ukrainian',
+    ua: 'Українська',
+  },
+  english: {
+    en: 'English',
+    ua: 'Англійська',
+  },
+  native: {
+    en: 'Native',
+    ua: 'рідна',
+  },
+  preInt: {
+    en: 'Pre-Intermediate',
+    ua: 'середній рівень',
+  },
+  dotsUkrainian: {
+    en: ' .......... ',
+    ua: ' ...... ',
+  },
+  dotsEnglish: {
+    en: ' ................ ',
+    ua: ' ...... ',
+  },
+  interests: {
+    en: 'Interests',
+    ua: 'Інтереси',
+  },
+  fitness: {
+    en: 'Fitness',
+    ua: 'Фітнес',
+  },
+  reading: {
+    en: 'Reading books',
+    ua: 'Читання книг',
+  },
+  it: {
+    en: 'IT',
+    ua: 'IT',
+  },
+  games: {
+    en: 'Board games',
+    ua: 'Настільні ігри',
+  },
+  description: {
+    en: 'I took part in writing more than 100 expert opinions. I wrote more than 50 publications on websites and social networks. In 2019, I completed the training and certification at the Ministry of Justice of Ukraine as an independent forensic expert in the specialty `Research of documents of financial and credit transactions`.',
+    ua: 'Взяла участь у написанні понад 100 експертних висновків. Написала понад 50 публікацій з розміщенням на сайтах та у соц.мережах. У 2019 році пройшла і успішно завершила підготовку та атестацію в Міністерстві юстиції України на незалежного судового експерта по спеціальності "Дослідження документів фінансово-кредитних операцій".',
+  },
+  projects: {
+    en: 'Work with sites',
+    ua: 'Робота із сайтами',
+  },
+  site: {
+    en: 'personal business page',
+    ua: 'власна бізнес-сторінка',
+  },
+  publication: {
+    en: 'publications, design',
+    ua: 'публікації, оформлення',
+  },
+  work: {
+    en: 'Work experience',
+    ua: 'Досвід роботи',
+  },
+  expertActivity: {
+    en: 'Independent forensic expert activity',
+    ua: 'Незалежна судово-експертна діяльність',
+  },
+  expert: {
+    en: 'Credit expert',
+    ua: 'Експерт по кредитам у',
+  },
+  ersu: {
+    en: '«Expert Research Service of Ukraine, Limited Liability Company»',
+    ua: 'ТОВ «Експертно-дослідна служба України»',
+  },
+  bankingActivity: {
+    en: 'Banking activity',
+    ua: 'Банківська діяльність',
+  },
+  vtb: {
+    en: '«VTB Bank» Public Joint Stock company',
+    ua: 'АТ «ВТБ Банк»',
+  },
+  ukrsotsbank: {
+    en: '«Ukrsotsbank» Joint-stock Commercial Bank',
+    ua: 'АТ «Укрсоцбанк»',
+  },
+  leadEconomist: {
+    en: 'Lead Economist',
+    ua: 'Провідний економіст в',
+  },
+  upToNow: {
+    en: 'up to now',
+    ua: 'до тепер',
+  },
+  education: {
+    en: 'Education',
+    ua: 'Освіта та професійна підготовка',
+  },
+  lawInstitute: {
+    en: 'Інститут права та післядипломної освіти Міністерства юстиції України',
+    ua: 'Інститут права та післядипломної освіти Міністерства юстиції України',
+  },
+  lawDescription: {
+    en: 'Підготовка з теоретичних, організаційних і процесуальних питань судової експертизи',
+    ua: 'Підготовка з теоретичних, організаційних і процесуальних питань судової експертизи',
+  },
+  researchInstitute: {
+    en: 'Київський науково-дослідний інститут судових експертиз (КНДІСЕ)',
+    ua: 'Київський науково-дослідний інститут судових експертиз (КНДІСЕ)',
+  },
+  researchDescription: {
+    en: 'Практична підготовка для присвоєння кваліфікації судового експерта із спеціальності «Дослідження документів фінансово-кредитних операцій»',
+    ua: 'Практична підготовка для присвоєння кваліфікації судового експерта із спеціальності «Дослідження документів фінансово-кредитних операцій»',
+  },
+  additionalEducation: {
+    en: 'Additional Education',
+    ua: 'Додаткова освіта',
+  },
+  kneu: {
+    en: 'Kyiv National Economic University',
+    ua: 'Київський національний економічний університет ім. В. Гетьмана',
+  },
+  management: {
+    en: 'Management of entrepreneurship',
+    ua: 'Менеджмент підприємницької діяльності',
+  },
+  dotsManagement: {
+    en: ' ..................... ',
+    ua: ' ................. ',
+  },
+  dotsBachelor: {
+    en: ' ................................................. ',
+    ua: ' ...................................................................... ',
+  },
+  magister: {
+    en: 'Magister of Economics',
+    ua: 'Магістр економіки',
+  },
+  bachelor: {
+    en: 'Bachelor of Economics, teacher of economics',
+    ua: 'Бакалавр економіки, викладач економіки',
+  },
+  enterprises: {
+    en: 'Economic of enterprises',
+    ua: 'Економіка підприємств',
+  },
+  // completed: {
+  //   en: 'Completed a 9-month course on front-end and a 2-month course on back-end.',
+  //   ua: 'Завершила 9-місячний курс front-end та 2-місячний курс back-end розробки.',
+  // },
+  // learned: {
+  //   en: 'Learned the workings of JavaScript, React.js, HTML, CSS, and others.',
+  //   ua: 'Навчилась працювати з JavaScript, React.js, HTML, CSS та з іншими технологіями.',
+  // },
+  // vsc: {
+  //   en: 'Became familiar with using popular coding technology such as Visual Studio Code.',
+  //   ua: 'Ознайомилась з використанням популярної технології кодування Visual Studio Code.',
+  // },
+  // built: {
+  //   en: 'Built several web applications using the technologies above. Three of them are team projects.',
+  //   ua: 'Створила кілька веб-додатків, використовуючи вищезазначені технології, з яких три – це командні проекти.',
+  // },
+  providedExpertise: {
+    en: 'Provided expertise for court or out-of-court. I wrote over 100 credit expertise reports.',
+    ua: 'Надала експертизу для судового та позасудового розгляду. Написала понад 100 звітів кредитної експертизи.',
+  },
+  managedExpertise: {
+    en: 'Managed content and created content marketing. I wrote over 50 specialized publications and articles, which I posted on the web and social networks.',
+    ua: 'Керувала контентом і створювала маркетинговий контент. Написала понад 50 спеціалізованих публікацій і статей, які розміщувала в Інтернеті та соціальних мережах.',
+  },
+  customer: {
+    en: 'Provided full customer support services.',
+    ua: 'Надавала повний спектр послуг підтримки клієнтів.',
+  },
+  calculation: {
+    en: 'Developed a calculation system in Excel and adapted it for the majority of credit expertise reports.',
+    ua: 'Розробила систему розрахунків в Excel та адаптувала її для більшості звітів кредитної експертизи.',
+  },
+  analyzedCourt: {
+    en: 'Analyzed versatile court decisions from various instances.',
+    ua: 'Проаналізувала різноманітні судові рішення різних інстанцій.',
+  },
+  certification: {
+    en: 'Completed the certification in the Ministry of Justice of Ukraine as an independent forensic expert, specialty "Research of documents of financial and credit transactions".',
+    ua: 'Пройшла атестацію в Міністерстві юстиції України на звання незалежного судового експерта зі спеціальності «Дослідження документів фінансово-кредитних операцій».',
+  },
+};
